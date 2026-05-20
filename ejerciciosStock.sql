9bf8cb7dd26b5bd751afc655c825ef4af4c0756d
TanaDiaz <alumno27.tana.felipe@ipm.edu.ar>
2026-05-20 09:54:22 -0300

    Ej cursores stock

--- ejerciciosStock.sql
+++ ejerciciosStock.sql
@@ -9,3 +9,46 @@ limit 1;
 end//
 
 delimiter ;
+
+
+
+
+
+2)) Crear un trigger que antes de borrar en la tabla IngresoStock borre todas las filas de la tabla
+IngresoStock_Producto.
+
+delimiter //
+create trigger borrarFilasStock before delete on IngresoStock for each row
+begin
+delete from IngresoStock_Producto;
+end//
+delimiter ;
+
+
+
+3) Imaginando que agregamos una columna categoría en la tabla de clientes, hacer un trigger que, cada vez
+que se agrega un pedido, se calcule el monto total gastado por ese cliente en los últimos dos años y
+actualice la categoría del cliente. Las categorías son “bronce” hasta $50.000 inclusive, “ plata”de $50.000 a
+$100.000 inclusive y “oro” más de $100.000.
+
+
+delimiter //
+create trigger CalculoCategoria before insert on Pedidos for each row
+begin
+declare estado varchar(500);
+declare monto int;
+select montoGastado into monto from Pedidosg
+
+end//
+delimiter ;
+
+
+
+
+
+
+
+
+
+
+
